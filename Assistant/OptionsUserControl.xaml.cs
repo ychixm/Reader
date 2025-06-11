fb9222d3da5caa86b4ed9956c060b48e0ef95fa0
ychixm <yochix.M@hotmail.fr>
2025-06-11 11:19:59 +0200

    refacto files and theme refacto

---
+++ Assistant/OptionsUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Assistant
+{
+    /// <summary>
+    /// Interaction logic for OptionsUserControl.xaml
+    /// </summary>
+    public partial class OptionsUserControl : UserControl
+    {
+        public OptionsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
